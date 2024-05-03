5cedd19da39ca3652dec2b9a209e64bb5c20b0ae
Elisha Riedlinger <elisha@novicemail.com>
2024-05-02 22:34:43 -0700

    Add support for Visual Studio 2022 compiler

--- config.h
+++ config.h
@@ -1,119 +1,119 @@
 /* Define if deprecated EAX extensions are enabled */
-#cmakedefine ALSOFT_EAX
+#define ALSOFT_EAX
 
 /* Define if HRTF data is embedded in the library */
-#cmakedefine ALSOFT_EMBED_HRTF_DATA
+#define ALSOFT_EMBED_HRTF_DATA
 
 /* Define if we have the posix_memalign function */
-#cmakedefine HAVE_POSIX_MEMALIGN
+/* #undef HAVE_POSIX_MEMALIGN */
 
 /* Define if we have the _aligned_malloc function */
-#cmakedefine HAVE__ALIGNED_MALLOC
+#define HAVE__ALIGNED_MALLOC
 
 /* Define if we have the proc_pidpath function */
-#cmakedefine HAVE_PROC_PIDPATH
+/* #undef HAVE_PROC_PIDPATH */
 
 /* Define if we have the getopt function */
-#cmakedefine HAVE_GETOPT
+/* #undef HAVE_GETOPT */
 
 /* Define if we have DBus/RTKit */
-#cmakedefine HAVE_RTKIT
+/* #undef HAVE_RTKIT */
 
 /* Define if we have SSE CPU extensions */
-#cmakedefine HAVE_SSE
-#cmakedefine HAVE_SSE2
-#cmakedefine HAVE_SSE3
-#cmakedefine HAVE_SSE4_1
+#define HAVE_SSE
+#define HAVE_SSE2
+#define HAVE_SSE3
+#define HAVE_SSE4_1
 
 /* Define if we have ARM Neon CPU extensions */
-#cmakedefine HAVE_NEON
+/* #undef HAVE_NEON */
 
 /* Define if we have the ALSA backend */
-#cmakedefine HAVE_ALSA
+/* #undef HAVE_ALSA */
 
 /* Define if we have the OSS backend */
-#cmakedefine HAVE_OSS
+/* #undef HAVE_OSS */
 
 /* Define if we have the PipeWire backend */
-#cmakedefine HAVE_PIPEWIRE
+/* #undef HAVE_PIPEWIRE */
 
 /* Define if we have the Solaris backend */
-#cmakedefine HAVE_SOLARIS
+/* #undef HAVE_SOLARIS */
 
 /* Define if we have the SndIO backend */
-#cmakedefine HAVE_SNDIO
+/* #undef HAVE_SNDIO */
 
 /* Define if we have the WASAPI backend */
-#cmakedefine HAVE_WASAPI
+#define HAVE_WASAPI
 
 /* Define if we have the DSound backend */
-#cmakedefine HAVE_DSOUND
+#define HAVE_DSOUND
 
 /* Define if we have the Windows Multimedia backend */
-#cmakedefine HAVE_WINMM
+#define HAVE_WINMM
 
 /* Define if we have the PortAudio backend */
-#cmakedefine HAVE_PORTAUDIO
+/* #undef HAVE_PORTAUDIO */
 
 /* Define if we have the PulseAudio backend */
-#cmakedefine HAVE_PULSEAUDIO
+/* #undef HAVE_PULSEAUDIO */
 
 /* Define if we have the JACK backend */
-#cmakedefine HAVE_JACK
+/* #undef HAVE_JACK */
 
 /* Define if we have the CoreAudio backend */
-#cmakedefine HAVE_COREAUDIO
+/* #undef HAVE_COREAUDIO */
 
 /* Define if we have the OpenSL backend */
-#cmakedefine HAVE_OPENSL
+/* #undef HAVE_OPENSL */
 
 /* Define if we have the Oboe backend */
-#cmakedefine HAVE_OBOE
+/* #undef HAVE_OBOE */
 
 /* Define if we have the Wave Writer backend */
-#cmakedefine HAVE_WAVE
+#define HAVE_WAVE
 
 /* Define if we have the SDL2 backend */
-#cmakedefine HAVE_SDL2
+/* #undef HAVE_SDL2 */
 
 /* Define if we have dlfcn.h */
-#cmakedefine HAVE_DLFCN_H
+/* #undef HAVE_DLFCN_H */
 
 /* Define if we have pthread_np.h */
-#cmakedefine HAVE_PTHREAD_NP_H
+/* #undef HAVE_PTHREAD_NP_H */
 
 /* Define if we have malloc.h */
-#cmakedefine HAVE_MALLOC_H
+#define HAVE_MALLOC_H
 
 /* Define if we have cpuid.h */
-#cmakedefine HAVE_CPUID_H
+/* #undef HAVE_CPUID_H */
 
 /* Define if we have intrin.h */
-#cmakedefine HAVE_INTRIN_H
+#define HAVE_INTRIN_H
 
 /* Define if we have guiddef.h */
-#cmakedefine HAVE_GUIDDEF_H
+#define HAVE_GUIDDEF_H
 
 /* Define if we have initguid.h */
-#cmakedefine HAVE_INITGUID_H
+/* #undef HAVE_INITGUID_H */
 
 /* Define if we have GCC's __get_cpuid() */
-#cmakedefine HAVE_GCC_GET_CPUID
+/* #undef HAVE_GCC_GET_CPUID */
 
 /* Define if we have the __cpuid() intrinsic */
-#cmakedefine HAVE_CPUID_INTRINSIC
+#define HAVE_CPUID_INTRINSIC
 
 /* Define if we have SSE intrinsics */
-#cmakedefine HAVE_SSE_INTRINSICS
+#define HAVE_SSE_INTRINSICS
 
 /* Define if we have pthread_setschedparam() */
-#cmakedefine HAVE_PTHREAD_SETSCHEDPARAM
+/* #undef HAVE_PTHREAD_SETSCHEDPARAM */
 
 /* Define if we have pthread_setname_np() */
-#cmakedefine HAVE_PTHREAD_SETNAME_NP
+/* #undef HAVE_PTHREAD_SETNAME_NP */
 
 /* Define if we have pthread_set_name_np() */
-#cmakedefine HAVE_PTHREAD_SET_NAME_NP
+/* #undef HAVE_PTHREAD_SET_NAME_NP */
 
 /* Define the installation data directory */
-#cmakedefine ALSOFT_INSTALL_DATADIR "@ALSOFT_INSTALL_DATADIR@"
+/* #undef ALSOFT_INSTALL_DATADIR */
