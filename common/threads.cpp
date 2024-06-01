6d05bce15e022d627aae7090ef4a07d4ffab3005
Elisha Riedlinger <elisha@novicemail.com>
2024-05-31 18:59:48 -0700

    Don't raise exception

--- common/threads.cpp
+++ common/threads.cpp
@@ -50,11 +50,11 @@ void althrd_setname(const char *name)
     info.dwThreadID = ~DWORD{0};
     info.dwFlags = 0;
 
-    __try {
-        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info);
-    }
-    __except(EXCEPTION_CONTINUE_EXECUTION) {
-    }
+    //__try {
+    //    RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+    //}
+    //__except(EXCEPTION_CONTINUE_EXECUTION) {
+    //}
 #undef MS_VC_EXCEPTION
 
 #else
